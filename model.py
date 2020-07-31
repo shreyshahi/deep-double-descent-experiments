fixed a bug with normalization, training changes

model.py
@@ -8,21 +8,12 @@ class PolynomialModel(nn.Module):
         self.order = order
         self.layer = nn.Linear(order, 1)
         self.layer.weight.data.uniform_(-1e-3, 1e-3)
-        self.mean, self.sd = self.get_normalization_factors()
-
-    def get_normalization_factors(self):
-        d = torch.range(-2 * 3.14, 2 * 3.14, 0.005).unsqueeze(1)
-        dd = torch.cat(
-            [d**(p+1) for p in range(self.order)],
-            dim=1
-        ) / (2 * 3.14) # force x to lie between -1 and 1
-        return(torch.mean(dd, dim=0), torch.std(dd, dim=0))
 
     def forward(self, x):
+        x = x / (2 * 3.14) # forces everything between -1 and 1
         input_data = torch.cat(
             [x**(p+1) for p in range(self.order)],
             dim=1
-        ) / (2 * 3.14)
-        input_data = (input_data - self.mean) / self.sd
+        )
         return self.layer(input_data)
 

train_parallel.py
@@ -16,7 +16,7 @@ def log_preds(order, epoch, x, y, pred_file):
 
 
 def train_and_log_models(order):
-    epochs = 500000
+    epochs = 100000
     model = PolynomialModel(order=order + 1)
     log_file_format = "./logs/run_log{}.csv"
     log_file = open(log_file_format.format(model.order), "w")
@@ -28,7 +28,7 @@ def train_and_log_models(order):
     )
     test_x, test_y = test.get_all_data()
     criterion = torch.nn.MSELoss(reduction="sum")
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     for epoch in range(epochs):
         train_err = 0
         for train_x, train_y in iter(train_data_loader):